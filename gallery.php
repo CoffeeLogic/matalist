3381656bc9563fb631c64e0fd249eff6740b5272
darryllp <darryllpascua@gmail.com>
2019-04-10 23:31:53 -0700

    lol

--- gallery.php
+++ gallery.php
@@ -173,7 +173,11 @@
     
 </main><!--MAIN-->
 
-    
+<div>
+      <?php
+      echo '<span class="push-right"> Logged in as '.$_SESSION['uid'].'</span>';
+      ?>
+      </div>
 
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
