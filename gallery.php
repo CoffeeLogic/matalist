4831138ff7822f83bfbb58e256891118ce173160
Brandon <brandon@Brandons-MacBook-Air.local>
2019-04-14 16:20:47 -0700

    take out extra code pls?

--- gallery.php
+++ gallery.php
@@ -147,7 +147,7 @@
               }
             }
           echo '</div>';
-          /////////////////////////////////
+          ////////////////////////////////
           }//if
         else
           {
@@ -156,31 +156,6 @@
         ?>
       </div>
     <!--Logged in message & logout-->
-
-          <!------------------
-          <div class="gallery-container">
-            <?php
-            include_once 'includes/dbh.inc.php';
-
-            $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC;";
-            $stmt = mysqli_stmt_init($conn);
-            if (!mysqli_stmt_prepare($stmt, $sql)) {
-              echo "SQL statement failed!";
-            } else {
-              mysqli_stmt_execute($stmt);
-              $result = mysqli_stmt_get_result($stmt);
-
-              while ($row = mysqli_fetch_assoc($result)) {
-                echo '<a href="#">
-                  <div style="background-image: url(/img/gallery/'.$row["imgFullNameGallery"].');"></div>
-                  <h3>'.$row["titleGallery"].'</h3>
-                  <p>'.$row["descGallery"].'</p>
-                </a>';
-              }
-            }
-            ?>
-          </div>
-          ----------------------->
 
 
         </div>
