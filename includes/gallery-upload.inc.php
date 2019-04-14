d1265aea571a1e03e1d9ad8bace489b339c123f9
Brandon <brandon@Brandons-MacBook-Air.local>
2019-04-14 14:55:11 -0700

    change file extenstions

--- includes/gallery-upload.inc.php
+++ includes/gallery-upload.inc.php
@@ -22,7 +22,7 @@ if (isset($_POST['submit'])) {
   $fileExt = explode(".", $fileName);
   $fileActualExt = strtolower(end($fileExt));
 
-  $allowed = array("jpg", "jpeg", "png");
+  $allowed = array(".jpg", ".jpeg", ".png");
 
   if (in_array($fileActualExt, $allowed)) {
     if ($fileError === 0) {
