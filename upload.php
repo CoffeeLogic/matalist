9d3764c2f392e20a4e8b95f830b0ad23e427f7ab
Brandon <brandon@Brandons-MacBook-Air.local>
2019-04-14 17:06:16 -0700

    add /

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 
 $link = mysql_connect("Matalistdb.cbhzd1fnevha.us-east-2.rds.amazonaws.com", "matalist", "csunmatalist");
-$target_dir = "img/gallery";
+$target_dir = "img/gallery/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 /*$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
