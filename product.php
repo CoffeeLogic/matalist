0ea2b7792fdf0afd6674dbb47051a896204dcd37
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-15 23:42:12 -0700

    Update product.php

--- product.php
+++ product.php
@@ -1,1 +1,152 @@
-cfcfc
+<?php include 'header.php'?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Craigslist Dashboard</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css'>
+  <link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:400,100,700,300'>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto+Slab'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/buttoncss.css">
+  
+</head>
+<body>
+<nav class="app--nav state--loaded">
+  <!--Logo-->
+  <a href="#" class="app--logo">MataList</a>
+  <!--Side nav filters-->
+  <div class="rslt__fltr">  
+      <!--area filter-->
+     <div class="rslt__fltr ui-nav-menu state--expanded">
+      <button class="ui-nav-menu-trgr">
+        <i class="btn__seg ion ion-ios-location"></i><span class="btn__seg btn__seg--txt">CSUN</span>
+      </button>
+      <ul class="ui-nav-menu-opts">
+        <li class="menu__fltr fltr-type__row">
+          <span class="menu__fltr__lgnd lgnd-clr--1"></span>
+          <span class="menu__fltr__lbl"><a href="index.php" title="Index">Index</a></span>
+        </li>
+         <li class="menu__fltr fltr-type__row">
+          <span class="menu__fltr__lgnd lgnd-clr--1"></span>
+          <span class="menu__fltr__lbl"><a href="login.php" title="Login">Login</a></span>
+        </li>
+        <li class="menu__fltr fltr-type__row">
+          <span class="menu__fltr__lgnd lgnd-clr--1"></span>
+          <span class="menu__fltr__lbl"><a href="signup.php" title="Signup">Signup</a></span>
+        </li>
+         <li class="menu__fltr fltr-type__row">
+          <span class="menu__fltr__lgnd lgnd-clr--1"></span>
+          <span class="menu__fltr__lbl"><a href="gallery.php" title="Gallery">Gallery</a></span>
+        </li>
+      </ul>
+    </div>
+    <!----------------------Tag checkboxes------------------->
+    <div class="rslt__fltr ui-nav-menu state--expanded" js-ui-menu>
+      <button class="ui-nav-menu-trgr" js-ui-menu-trigger>
+        <i class="btn__seg ion ion-pricetag"></i><span class="btn__seg btn__seg--txt">for sale</span><i class="btn__seg ion ion-ios-arrow-down menu-arrow"></i>
+      </button>
+      <ul class="ui-nav-menu-opts">
+        <li class="menu__fltr fltr-type__row">
+          <input type="checkbox" name="sale" value="cars & trucks" />
+          <span class="menu__fltr__lbl">Books</span>
+          <span class="menu__fltr__cntr cntr-clr--1">0</span>
+        </li>        
+        <li class="menu__fltr fltr-type__row">
+          <input type="checkbox" name="sale" value="auto parts" />
+          <span class="menu__fltr__lbl">Tutoring</span>
+          <span class="menu__fltr__cntr cntr-clr--2">0</span>
+        </li>
+        <li class="menu__fltr fltr-type__row">
+          <input type="checkbox" name="sale" value="wanted" />
+          <span class="menu__fltr__lbl">Dorm goods</span>
+          <span class="menu__fltr__cntr cntr-clr--3">0</span>
+        </li>
+        <li class="menu__fltr fltr-type__row">
+          <input type="checkbox" name="sale" value="wanted" />
+          <span class="menu__fltr__lbl">Food</span>
+          <span class="menu__fltr__cntr cntr-clr--4">0</span>
+        </li>
+      </ul>
+    </div>
+    <!----------------------Tag checkboxes------------------->
+  </div>
+</nav>
+<!-------------MAIN GALLERY------------>
+<main class="app--core">
+  <h2 class="app--logo">Gallery - CSUN</h2>
+  <header class="app--hdr">
+    <div class="hdr__inr">
+      <section class="buttons">
+        <div class="buttoncont">
+          <a href="login.php" class="headerbtn headerbtn-1">Login</a>
+          <a href="aboutus.html" class="headerbtn headerbtn-1">About Us
+          <a href="submit.php" class="headerbtn headerbtn-1">Post</a></a>
+        </div>
+      </section>
+      <div class="hdr__acts">
+        <button class="btn__rd btn--fav-cnt" onclick="window.location.href='includes/logout.php'">
+          <i class="btn__seg ion-android-favorite"></i>
+        </button>
+        <button class="btn__rd btn--usr-avtr" onclick="window.location.href='submit.php'"></button>
+      </div>
+    </div>
+  </header>
+       <section class="gallery-links">
+        <div class="wrapper">
+          <h2>Product</h2>
+<br>
+    <!--Logged in message & logout-->
+    <div align="center">   
+      <?php
+        if(isset($_SESSION["uid"]))
+          {
+            $uid = $_SESSION["uidUsers"];
+            echo "Logged in as '{$_SESSION['uid']}' <br> <a href=includes/logout.php>Logout</a>";
+            //////////////////////////////////////
+            echo '<div class="gallery-container">';
+            include_once 'includes/dbh.inc.php';
+            $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC;";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+              echo "SQL statement failed!";
+            } else {
+              mysqli_stmt_execute($stmt);
+              $result = mysqli_stmt_get_result($stmt);
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo '<a href="#">
+                  <div style="background-image: url(img/'.$row["imgFullNameGallery-1"].');"></div>
+                  <h3>'.$row["titleGallery"].'</h3>
+                  <p>'.$row["descGallery"].'</p>
+                </a>';
+              }
+            }
+          echo '</div>';
+          ////////////////////////////////
+          }//if
+        else
+          {
+            echo "Please log in to view gallery.";
+          }//else
+        ?>
+      </div>
+    <!--Logged in message & logout-->
+
+
+        </div>
+      </section>
+    
+</main><!--MAIN-->
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js'></script>
+<script  src="js/index.js"></script>
+
+
+</body>
+</html>
