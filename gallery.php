f9ba46aa58555e48bb4403bbe80585196ea8a2ae
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-17 01:55:20 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -142,7 +142,9 @@
               $result = mysqli_stmt_get_result($stmt);
 
               while ($row = mysqli_fetch_assoc($result)) {
-                echo '<a href="product.php">
+                $item_id = $row["imgFullNameGallery"];
+                echo <a href="product.php">
+                echo <varname="$item_id">
                   <div style="background-image: url(img/'.$row["imgFullNameGallery"].');"></div>
                   <h3>'.$row["titleGallery"].'</h3>
                   <p>'.$row["descGallery"].'</p>
@@ -157,14 +159,7 @@
             echo "Please log in to view gallery.";
           }//else
         ?>
-      <?php
-      while ($row = mysqli_fetch_assoc($result)) {
-              $item_id = $row["imgFullNameGallery"];
-                echo '<a href="product.php?varname=$item_id">
-                  <div style="background-image: url(img/'.$row["imgFullNameGallery"].');"></div>
-                  <h3>'.$row["titleGallery"].'</h3>
-                  <p>'.$row["descGallery"].'</p>
-         ?>
+     
       </div>
     <!--Logged in message & logout-->
 
