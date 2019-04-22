30415d6cf36f3001740ca70011a58a0ab0b9a74e
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-22 01:48:54 -0700

    Update product.php

--- product.php
+++ product.php
@@ -46,33 +46,7 @@
       </ul>
     </div>
     <!----------------------Tag checkboxes------------------->
-    <div class="rslt__fltr ui-nav-menu state--expanded" js-ui-menu>
-      <button class="ui-nav-menu-trgr" js-ui-menu-trigger>
-        <i class="btn__seg ion ion-pricetag"></i><span class="btn__seg btn__seg--txt">for sale</span><i class="btn__seg ion ion-ios-arrow-down menu-arrow"></i>
-      </button>
-      <ul class="ui-nav-menu-opts">
-        <li class="menu__fltr fltr-type__row">
-          <input type="checkbox" name="sale" value="cars & trucks" />
-          <span class="menu__fltr__lbl">Books</span>
-          <span class="menu__fltr__cntr cntr-clr--1">0</span>
-        </li>        
-        <li class="menu__fltr fltr-type__row">
-          <input type="checkbox" name="sale" value="auto parts" />
-          <span class="menu__fltr__lbl">Tutoring</span>
-          <span class="menu__fltr__cntr cntr-clr--2">0</span>
-        </li>
-        <li class="menu__fltr fltr-type__row">
-          <input type="checkbox" name="sale" value="wanted" />
-          <span class="menu__fltr__lbl">Dorm goods</span>
-          <span class="menu__fltr__cntr cntr-clr--3">0</span>
-        </li>
-        <li class="menu__fltr fltr-type__row">
-          <input type="checkbox" name="sale" value="wanted" />
-          <span class="menu__fltr__lbl">Food</span>
-          <span class="menu__fltr__cntr cntr-clr--4">0</span>
-        </li>
-      </ul>
-    </div>
+    
     <!----------------------Tag checkboxes------------------->
   </div>
 </nav>
