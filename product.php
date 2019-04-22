023a1f8450059f11fff772e247623829035ab4d7
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-22 02:07:34 -0700

    Update product.php

--- product.php
+++ product.php
@@ -57,7 +57,7 @@
               $row = mysqli_fetch_assoc($result);
               
               echo '<a href="gallery.php">
-              <div align="center">
+              <div align="left">
               <h1>'.$row["titleGallery"].'</h1>
               <br>
               <p>'.$row["users_email"].'</p>
