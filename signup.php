674bc3e1c626211b44f496b9efdc8dc0c88f7e85
darryllp <darryllpascua@gmail.com>
2019-04-26 03:18:14 -0700

    lol

--- signup.php
+++ signup.php
@@ -188,6 +188,7 @@
             <input type="password" name="pwd-repeat" placeholder="Repeat password">
             <!-- Recaptcha -->
             <center><div class="g-recaptcha" data-sitekey="6LeUYaAUAAAAAMHBRgXaugN7WkfgN0jl77UIFixM"></div></center>
+            <br>
             <button type="submit" name="signup-submit">Signup</button>
             <br>
             <center><a href="login.php">Already a member? Log In</a></center>
