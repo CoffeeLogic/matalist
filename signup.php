0db0b97b656bbe4b18425613cae80c244e5055fa
darryllp <darryllpascua@gmail.com>
2019-04-26 14:50:15 -0700

    lol

--- signup.php
+++ signup.php
@@ -151,20 +151,24 @@
          
 
           <?php
-           if (isset($_GET['signup'])) {
-            $username = $_GET['username'];
+          // CAPTCHA STUFF
+           if (isset($_POST['signup'])) {
+            $username = $_POST['username'];
             $secretKey = "6LeUYaAUAAAAALAOD0RyJglYHbO8xz7y3wqPhcX1";
-            $responseKey = $_GET['g-recaptcha-response'];
+            $responseKey = $_POST['g-recaptcha-response'];
             $userIP = $_SERVER['REMOTE_ADDR'];
           
             $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$userIP";
             $response = file_get_contents($url);
             $response = json_decode($response);
-            if ($response->success)
+            if ($response->success){
                 echo '<p class="signupsuccess">Verification success!</p>';
-            else
+            }
+            else{
                 echo '<p class="signuperror"> Verification failed!</p>';
+            }
           }
+
           // Here we create an error message if the user made an error trying to sign up.
           if (isset($_GET["error"])) {
             if ($_GET["error"] == "emptyfields") {
