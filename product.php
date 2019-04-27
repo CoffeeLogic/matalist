7c08dc89939d0f6afaa17709301e3bc288d5c85c
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-27 06:05:31 -0700

    Update product.php

--- product.php
+++ product.php
@@ -67,8 +67,7 @@
 
               
               
-              
-                  <span style="">
+  
                   <div style=" background-image: url(img/'.$row["imgFullNameGallery"].'); height: 400px; width: 800px;"</div>
                   
                   <p><font size="10" face="arial" color="black">'.$row["titleGallery"].'</font><p>
@@ -80,7 +79,7 @@
                   <br>
                   <p><font size="4" face="arial" color="blue">Contact:'.$row["users_email"].'</font></p>
                   <p><font size="4" face="arial" color="red">Gallery ID:'.$row["idGallery"].'</font></p>
-                  </span>
+                  
                   </div>
                 </a>';   
               
