1f59ba4a4f3b73e952678767f7c7df7609044348
Vincent Lee <45250935+CoffeeLogic@users.noreply.github.com>
2019-04-27 12:44:36 -0700

    Add files via upload

--- submit.php
+++ submit.php
@@ -37,7 +37,7 @@ $_SESSION['username'] = "Admin";
                     
                     <input type="text" name="filetitle" maxlength="35" placeholder="Post title">
                     <input type="text" name="filedesc" maxlength="200" placeholder="description">
-                    <input type="text" name="details" class="descriptionbox" placeholder="details...">
+                    <input type="textbox" name="details" class="descriptionbox" placeholder="details...">
                     <br>
 
                 </div>
