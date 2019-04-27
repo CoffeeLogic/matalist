7e05af96882fbd09aece83ac99440b3f1a973962
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-27 08:29:46 -0700

    Update product.php

--- product.php
+++ product.php
@@ -62,14 +62,12 @@
                
                
               echo '<a href="#" style="text-decoration:none;">
-              
-               <div align="center"> 
-                  
+                 
                   <div style=" background-image: url(img/'.$row["imgFullNameGallery"].'); height: 400px; width: 800px;"></div>
+                  <br>
                   <br>
                   <p><font size="10" face="arial" color="black">'.$row["titleGallery"].'</font><p>
                   <br>
-
                   <br>
                   <p><font size="4" face="arial" color="blue">Contact:'.$row["users_email"].'</font></p>
                   <p><font size="4" face="arial" color="red">Gallery ID:'.$row["idGallery"].'</font></p>
