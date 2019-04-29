90cd14c5b3a318030b8a0bc759d01a03a6550be7
Joselito Garcia <42253417+jgg99406@users.noreply.github.com>
2019-04-29 09:59:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,20 +16,7 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/buttoncss.css">
   <link rel="stylesheet" href="css/aboutus.css">	
-    
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <style>
-  .carousel-inner > .item > img,
-  .carousel-inner > .item > a> img {
-    width: 360px;
-    margin: 245px;
-  }
-  </style>
-
-  
+      
 </head>
 
 <body>
@@ -150,50 +137,7 @@ make it easier to access what events and fundraisers are happening on campus to 
 
 
 	  
-      <div class="container">
-  <br>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox" >
-
-      <div class="item active">
-        <img src="img/gallery/csun.jpg" alt="csun" width="260" height="145">
-      </div>
-
-      <div class="item">
-        <img src="img/gallery/capture (3).png" alt="matador" width="260" height="145">
-      </div>
-    
-      <div class="item">
-        <img src="img/gallery/jose.jpg" alt="Flower" width="260" height="145">
-      </div>
-
-      <div class="item">
-        <img src="img/gallery/garrett.jpg" alt="Flower" width="260" height="145">
-      </div>
-  
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  
-</div>
-	      </main>	    
+</main>	    
 </body>
     
 
