d6c4db196378a9563b293dc24b710e029a91fc22
Vincent Lee <45250935+CoffeeLogic@users.noreply.github.com>
2019-04-29 01:49:45 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -23,8 +23,9 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <style>
   .carousel-inner > .item > img,
-  .carousel-inner > .item > a> img {
-    width: 100%;
+  .carousel-inner > .item > a> img
+  {
+    width: 50%;
     margin: 0;
   }
   </style>
@@ -147,50 +148,53 @@ make it easier to access what events and fundraisers are happening on campus to 
 </div>
 	</div>
 
-  <div class="container">
-    <br>
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-      </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox" >
 
-        <div class="item active">
-          <img src="img/gallery/csun.jpg" alt="csun">
-        </div>
+	  
+      <div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
 
-        <div class="item">
-          <img src="img/gallery/capture (3).png" alt="matador">
-        </div>
-      
-        <div class="item">
-          <img src="img/gallery/jose.jpg" alt="Flower">
-        </div>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox" >
 
-        <div class="item">
-          <img src="img/gallery/garrett.jpg" alt="Flower">
-        </div>
-    
+      <div class="item active">
+        <img src="img/gallery/csun.jpg" alt="csun" width="260" height="145">
       </div>
 
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+      <div class="item">
+        <img src="img/gallery/capture (3).png" alt="matador" width="260" height="145">
+      </div>
     
-   </div>
-</main>	    
+      <div class="item">
+        <img src="img/gallery/jose.jpg" alt="Flower" width="260" height="145">
+      </div>
+
+      <div class="item">
+        <img src="img/gallery/garrett.jpg" alt="Flower" width="260" height="145">
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  
+</div>
+	      </main>	    
 </body>
     
 
