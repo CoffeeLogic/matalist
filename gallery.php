4920b86f4fdff2a056b1c3e10d8b8d32dd537c9c
Brandon <brandon@Brandons-MacBook-Air.local>
2019-05-01 23:49:03 -0700

    adsfasdfdsfa

--- gallery.php
+++ gallery.php
@@ -181,7 +181,7 @@ if ($_GET["login"] == "success") {
 
             $stmt = mysqli_stmt_init($conn);
             if (!mysqli_stmt_prepare($stmt, $sql)) {
-              echo "SQL statement failed!";
+              echo "SQL statement failed!3452345345";
             } else {
               mysqli_stmt_execute($stmt);
               $result = mysqli_stmt_get_result($stmt);
