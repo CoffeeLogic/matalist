3c28c97fd40187db4186b9220911178a0fc28c91
darryllp <darryllpascua@gmail.com>
2019-05-01 22:47:13 -0700

    lol

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -3,7 +3,7 @@
 if (isset($_POST['login-submit'])) {
     // We include the connection script so we can use it later.
     // We don't have to close the MySQLi connection since it is done automatically, but it is a good habit to do so anyways since this will immediately return resources to PHP and MySQL, which can improve performance.
-    require_once('/spook/dbh.inc.php');
+    require_once('spook/dbh.inc.php');
     // We grab all the data which we passed from the signup form so we can use it later.
     $mailuid = $_POST['mail'];
     $password = $_POST['pwd'];
