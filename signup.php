2b35b5b323f3b00a234620a1cf5fdde7616f55b3
Brandon <brandon@Brandons-MacBook-Air.local>
2019-05-03 01:03:49 -0700

    sdfgdfg

--- signup.php
+++ signup.php
@@ -255,6 +255,13 @@
       <br>
     <!--Logged in message & logout-->
 
+
+    <?php 
+  foreach ($_POST as $key => $value) {
+    echo '<p><strong>' . $key.':</strong> '.$value.'</p>';
+  }
+?>
+
 </main>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script>
