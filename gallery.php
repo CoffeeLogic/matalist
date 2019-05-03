ea828d2810457f89ce1c9f60b1a49a61fe6f709b
Brandon <brandon@Brandons-MacBook-Air.local>
2019-05-02 22:19:24 -0700

    i forgot what i did

--- gallery.php
+++ gallery.php
@@ -5,7 +5,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Gallery</title>
+  <title>Matalist - Gallery</title>
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 
--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Login</title>
+  <title>Matalist - Login</title>
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 
