4b8d3067032226d3aef98e49295a9d2c9892c2f1
Vincent Lee <45250935+CoffeeLogic@users.noreply.github.com>
2019-05-03 14:01:07 -0700

    Add files via upload

--- signup.php
+++ signup.php
@@ -174,6 +174,9 @@
             ?>
             <input type="password" name="pwd" placeholder="Password">
             <input type="password" name="pwd-repeat" placeholder="Repeat password">
+          
+            <div class="padding30"></div>
+
             <div class="g-recaptcha" data-sitekey="6LeUYaAUAAAAAMHBRgXaugN7WkfgN0jl77UIFixM"></div>
             <button type="submit" name="signup-submit">Signup</button>
             <br>
