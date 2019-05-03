5d1b87db10564afd177c020cfa211c691ebdf682
darryllp <darryllpascua@gmail.com>
2019-05-02 20:39:36 -0700

    nai

--- signup.php
+++ signup.php
@@ -140,9 +140,9 @@
           <h1><br>Signup Now</h1>
           <?php
           // CAPTCHA STUFF
-          if (isset($_POST['signup'])) {
+          if (isset($_GET['signup'])) {
             $secretKey = "6LeUYaAUAAAAALAOD0RyJglYHbO8xz7y3wqPhcX1";
-            $responseKey = $_POST['g-recaptcha-response'];
+            $responseKey = $_GET['g-recaptcha-response'];
             //$userIP = $_SERVER['REMOTE_ADDR'];
           
             $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey";
