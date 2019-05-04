e1d201520226007c21cb8d49da15857f0a8904ba
Vincent Lee <45250935+CoffeeLogic@users.noreply.github.com>
2019-05-03 18:24:45 -0700

    Add files via upload

--- gallery.php
+++ gallery.php
@@ -92,12 +92,11 @@
           <span class="menu__fltr__lbl">Food</span>
           <span class="menu__fltr__cntr cntr-clr--4">0</span>
         </li> 
-        <li
        
-          <div class="buttoncont">  
-          <input type="submit" value="Submit" class="headerbtn headerbtn-1">
-      
-          <a href="gallery.php" class="headerbtn headerbtn-1">Reset</a>
+          <div class="buttoncont">
+            <input type="submit" value="Submit" class="headerbtn headerbtn-1">
+        
+            <a href="gallery.php" class="headerbtn headerbtn-1">Reset</a>
      
           </div>
         </li>
