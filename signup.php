883238f0bfbd072789199a3a1e10ec0ae03d91e7
darryllp <darryllpascua@gmail.com>
2019-05-06 01:11:35 -0700

    saf

--- signup.php
+++ signup.php
@@ -174,7 +174,7 @@
             <input type="password" name="pwd" placeholder="Password">
             <input type="password" name="pwd-repeat" placeholder="Repeat password">
             <div>
-            <div class="clearfix g-recaptcha" data-sitekey="6LeUYaAUAAAAAMHBRgXaugN7WkfgN0jl77UIFixM" style="transform:scale(0.50);-webkit-transform:scale(0.80);transform-origin:0 0;-webkit-transform-origin:0 0;"></div>
+            <div class="clearfix g-recaptcha" data-sitekey="6LeUYaAUAAAAAMHBRgXaugN7WkfgN0jl77UIFixM" style="transform:scale(0.80);-webkit-transform:scale(0.80);transform-origin:0 0;-webkit-transform-origin:0 0;"></div>
             <div class="pad-all">
               <button type="submit" name="signup-submit">Signup</button>
             </div>
